faec1aef7c9b679c428b1bfa966a848cce50dde5
DylanSDev <dylansdiaz.dev@gmail.com>
2025-04-09 20:25:31 -0300

    Se agregó la clase Pais.

---
+++ src/domain/Pais.java
@@ -0,0 +1,14 @@
+
+package domain;
+
+public class Pais {
+    private String Nombre;
+
+    public Pais(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+}
